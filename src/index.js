be9c663ffe50d71ec3216c35e78545e97c3fa46b
Tasveer Singh <taz@tazsingh.com>
2016-01-23 23:43:05 -0500

    Rename npm package and setup export

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import reducerStore from "./reducerStore";
 import mapReducersToProps from "./mapReducersToProps";
 import addReducer from "./addReducer";
-import setup from "./setup"
+import setupIncrementalReduxReducers from "./setupIncrementalReduxReducers"
 
 // use a module.exports here for non-ES2015 consumers
 module.exports = {
   reducerStore
 , mapReducersToProps
 , addReducer
-, setup
+, setupIncrementalReduxReducers
 }
